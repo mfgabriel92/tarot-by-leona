64956315e01542be37e9c0641c5ccb1038da4d12
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-07-19 20:57:57 +0800

    feat: floating Messenger icon to message page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import {
   Shop,
   Testimonials,
 } from "@/app/components";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -16,6 +18,15 @@ export default function Home() {
       <Testimonials />
       <Shop />
       <Footer />
+      <Link href="https://m.me/61551377159397" target="_blank">
+        <Image
+          src="/assets/messenger.png"
+          width={64}
+          height={64}
+          alt="Tarot by Leona Messenger"
+          className="fixed bottom-24 right-24 z-30"
+        />
+      </Link>
     </>
   );
 }
